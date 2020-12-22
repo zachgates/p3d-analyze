436b2eacd7bf8a8712efb53d39ac086c9e281eaf
Zach Gates <thezachgates@gmail.com>
2020-12-22 14:09:06 -0600

    Update __init__.py

--- panda3d_analyze/__init__.py
+++ panda3d_analyze/__init__.py
@@ -1,30 +1,4 @@
 #!/usr/local/bin/python3.9
-"""
-MIT License
-
-Copyright (c) 2019 Nxt Games, LLC
-Written by Jordan Maxwell 
-08/06/2019
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 
 __all__ = ['PStatContextStack', 'PStatContext', 'analyze']
 
@@ -37,7 +11,7 @@ from typing import Callable, Mapping, Optional
 from panda3d import core as p3d
 
 
-class PStatContextStack(type):
+class _PStatContextStack(type):
     """
     Manage the PStatClient, and the PStatContext objects in use.
     """
@@ -87,7 +61,7 @@ class PStatContextStack(type):
             return None
 
 
-class PStatContextStack(metaclass = PStatContextStack):
+class PStatContextStack(metaclass = _PStatContextStack):
     pass
 
 
